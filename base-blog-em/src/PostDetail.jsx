d672c0c0b3a8cea20b77707a993998320158cd8f
Neelesh-10826278_LTIMGHCE <neelesh.bizoara@paramount.com>
2026-04-28 16:52:13 +0530

    ch 1 and ch 2

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,14 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
-export function PostDetail({ post }) {
+
+export function PostDetail({ post, deleteMutation, updateMutation }) {
   // replace with useQuery
-  const data = [];
+  
+
+  // const data = [];
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) return <div>Loading comments...</div>;
+  if (isError) return <div>Error loading comments.</div>;
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <div><button onClick={() => deleteMutation.mutate(post.id)}>Delete</button> 
+      {deleteMutation.isPending && <p className="loading">Deleting...</p>}
+      {deleteMutation.isError && <p className="error">Error deleting post.</p>}
+      {deleteMutation.isSuccess && <p className="success">Post deleted successfully.</p>}
+      </div>
+      <div><button onClick={() => updateMutation.mutate(post.id)}>Update title</button></div>
+      {updateMutation.isPending && <p className="loading">Updating...</p>}
+      {updateMutation.isError && <p className="error">Error updating post.</p>}
+      {updateMutation.isSuccess && <p className="success">Post updated successfully.</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
